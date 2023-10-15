00acaff71909ff6e0b2a4246ac39a8642fc4d51d
Đặng Hoàn Phương <146814457+DangHoanPhuong@users.noreply.github.com>
2023-10-15 23:09:37 +0700

    Thêm phần Statistical cho trang Personal

--- lib/components/personal.dart
+++ lib/components/personal.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shoestore/components/statistical.dart';
 
 class Personal extends StatelessWidget {
   const Personal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Statistical(),
+    );
   }
 }
