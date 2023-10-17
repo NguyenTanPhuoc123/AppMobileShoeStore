a9032991697595df501612f774a0dd1222c3a70f
Viz <nghiemvi1234567890@gmail.com>
2023-10-17 21:53:55 +0700

    Update drawer.dart

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -23,9 +23,10 @@ class Menu extends StatelessWidget {
               ),
             ),
             
-            ListTile(
-              title: Text("Trang chủ",style: TextStyle(color: (idx == 0 ? Colors.blueAccent:Colors.grey),),),
-              leading: Icon(Icons.home,color: (idx == 0 ? Colors.blueAccent:Colors.grey),),
+            
+             ListTile(
+              title: Text("Cài đặt",style: TextStyle(color: (idx == 0 ? Colors.blueAccent:Colors.grey),),),
+              leading:Icon(Icons.settings,color: (idx == 0 ? Colors.blueAccent:Colors.grey)),
               onTap: ()
               {
                 if(idx!=0)
@@ -35,9 +36,9 @@ class Menu extends StatelessWidget {
                 }
               },
             ),
-             ListTile(
-              title: Text("Giỏ hàng",style: TextStyle(color: (idx == 1 ? Colors.blueAccent:Colors.grey),),),
-              leading:Icon(Icons.shopping_basket_rounded,color: (idx == 1 ? Colors.blueAccent:Colors.grey)),
+            ListTile(
+              title: Text("Hỗ trợ",style: TextStyle(color: (idx == 1 ? Colors.blueAccent:Colors.grey),),),
+              leading:Icon(Icons.support_agent,color: (idx == 1 ? Colors.blueAccent:Colors.grey)),
               onTap: ()
               {
                 if(idx!=1)
@@ -47,48 +48,12 @@ class Menu extends StatelessWidget {
                 }
               },
             ),
-             ListTile(
-              title: Text("Tin nhắn",style: TextStyle(color: (idx == 2 ? Colors.blueAccent:Colors.grey),),),
-              leading:Icon(Icons.message_rounded,color: (idx == 2 ? Colors.blueAccent:Colors.grey)),
+            ListTile(
+              title: Text("Đăng xuất",style: TextStyle(color: (idx == 2 ? Colors.blueAccent:Colors.grey),),),
+              leading:Icon(Icons.logout_rounded,color: (idx == 2 ? Colors.blueAccent:Colors.grey)),
               onTap: ()
               {
                 if(idx!=2)
-                {
-                  Navigator.popUntil(context, (route) => route.isFirst);
-                  Navigator.pushNamed(context, '/');
-                }
-              },
-            ),
-            ListTile(
-              title: Text("Tài khoản",style: TextStyle(color: (idx == 3 ? Colors.blueAccent:Colors.grey),),),
-              leading:Icon(Icons.account_box_rounded,color: (idx == 3 ? Colors.blueAccent:Colors.grey)),
-              onTap: ()
-              {
-                if(idx!=3)
-                {
-                  Navigator.popUntil(context, (route) => route.isFirst);
-                  Navigator.pushNamed(context, '/');
-                }
-              },
-            ),
-            ListTile(
-              title: Text("Cài đặt",style: TextStyle(color: (idx == 4 ? Colors.blueAccent:Colors.grey),),),
-              leading:Icon(Icons.settings,color: (idx == 4 ? Colors.blueAccent:Colors.grey)),
-              onTap: ()
-              {
-                if(idx!=4)
-                {
-                  Navigator.popUntil(context, (route) => route.isFirst);
-                  Navigator.pushNamed(context, '/');
-                }
-              },
-            ),
-            ListTile(
-              title: Text("Đăng xuất",style: TextStyle(color: (idx == 5 ? Colors.blueAccent:Colors.grey),),),
-              leading:Icon(Icons.logout_rounded,color: (idx == 5 ? Colors.blueAccent:Colors.grey)),
-              onTap: ()
-              {
-                if(idx!=5)
                 {
                   Navigator.popUntil(context, (route) => route.isFirst);
                   Navigator.pushNamed(context, '/');
