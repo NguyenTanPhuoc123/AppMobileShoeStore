a3dbe3d32c6006110dea33ed9eba67371da61982
Viz <nghiemvi1234567890@gmail.com>
2023-10-17 23:06:10 +0700

    mainScreen và components

---
+++ lib/components/personal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Personal extends StatelessWidget {
+  const Personal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoestore/mainScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       
-      
+     home: const MainScreen() 
     );
   }
 }
