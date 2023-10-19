2313b223bc93e6daf885c951a2d631038b3d513d
Tấn Phước Nguyễn <ngguyentanphuoc2003@gmail.com>
2023-10-19 09:18:22 +0700

    update statistical

--- lib/components/personal.dart
+++ lib/components/personal.dart
@@ -7,70 +7,68 @@ class Personal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(
       appBar: AppBar(
         title: const Text('Thông Tin Cá Nhân'),
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              backgroundImage:const AssetImage('assets/avatar1.jpg'),
-              radius: 80.0,
-              child: Container(
-                  decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.black, width: 2.0),
-                ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundImage:const AssetImage('assets/avatar1.jpg'),
+            radius: 80.0,
+            child: Container(
+                decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.black, width: 2.0),
               ),
             ),
-             const SizedBox(height: 20.0),
-            Container(
-              decoration: BoxDecoration(
-              border: Border.all(
-              color: Colors.black,
-              width: 2.0,
-             ),
-             borderRadius: BorderRadius.circular(10.0),
-             ),
-              child: const Column(
-                children: [
-                  UserInfoItem(text: 'Tên hiển thị:', value: 'Duy  '),
-                  UserInfoItem(text: 'Tên tài khoản:', value: 'DuyHuynh  '),
-                  UserInfoItem(text: 'Ngày sinh:', value: '1/1/2002  '),
-                  UserInfoItem(text: 'Số điện thoại:', value: '0998877666  '),
-                  UserInfoItem(text: 'Email:', value: 'duy@gmail.com  '),
-                ],
-              ),
-            ),
-      
-        Container(
-          child: TextButton(
-            onPressed: () {
-              Navigator.popUntil(context, (route) => route.isFirst);
-              Navigator.pushNamed(context, '/order');
-            },
-            child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Icon(Icons.shopping_cart_outlined),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Text("Danh sách đơn hàng"),
-                    ],
-                  ),
-                  Icon(Icons.arrow_forward_ios_sharp)
-                ]),
           ),
-        )
+           const SizedBox(height: 20.0),
+          Container(
+            decoration: BoxDecoration(
+            border: Border.all(
+            color: Colors.black,
+            width: 2.0,
+           ),
+           borderRadius: BorderRadius.circular(10.0),
+           ),
+            child: const Column(
+              children: [
+                UserInfoItem(text: 'Tên hiển thị:', value: 'Duy  '),
+                UserInfoItem(text: 'Tên tài khoản:', value: 'DuyHuynh  '),
+                UserInfoItem(text: 'Ngày sinh:', value: '1/1/2002  '),
+                UserInfoItem(text: 'Số điện thoại:', value: '0998877666  '),
+                UserInfoItem(text: 'Email:', value: 'duy@gmail.com  '),
+              ],
+            ),
+          ),
+          const Statistical(),
+      Container(
+        child: TextButton(
+          onPressed: () {
+            Navigator.popUntil(context, (route) => route.isFirst);
+            Navigator.pushNamed(context, '/order');
+          },
+          child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.shopping_cart_outlined),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text("Danh sách đơn hàng"),
+                  ],
+                ),
+                Icon(Icons.arrow_forward_ios_sharp)
+              ]),
+        ),
+      )
       ],
-    )
-    )     
+    ) ,
+        
   );           
 }
 }
@@ -95,11 +93,6 @@ class UserInfoItem extends StatelessWidget {
           Text(value),
         ],
       ),
-=======
-    return const Scaffold(
-      body: Statistical(),
->>>>>>> d81129fa953de4fa50503828e86cafc9b5a61e7e
     );
   }
 }
-
